89db694cbf92ae7fda3d94d9486652fb56295c1c
Covenant Ifeoluwa <covenantifeoluwa4@gmail.com>
2024-10-29 17:58:18 +0100

    "Added expo-font dependency to package.json and package-lock.json"

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import UserList from "./UserlIst";
+import UserList from "./Userlist";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    PoppinsThin: require("./assets/fonts/Poppins/Poppins-Thin.ttf"),
+    PoppinsThinItalic: require("./assets/fonts/Poppins/Poppins-ThinItalic.ttf"),
+    PoppinsLight: require("./assets/fonts/Poppins/Poppins-Light.ttf"),
+    PoppinsExtraLight: require("./assets/fonts/Poppins/Poppins-ExtraLight.ttf"),
+    PoppinsMedium: require("./assets/fonts/Poppins/Poppins-Medium.ttf"),
+    PoppinsMediumItalic: require("./assets/fonts/Poppins/Poppins-MediumItalic.ttf"),
+    PoppinsRegular: require("./assets/fonts/Poppins/Poppins-Regular.ttf"),
+    PoppinsSemiBold: require("./assets/fonts/Poppins/Poppins-SemiBold.ttf"),
+    PoppinsBold: require("./assets/fonts/Poppins/Poppins-Bold.ttf"),
+    PoppinsExtraBold: require("./assets/fonts/Poppins/Poppins-ExtraBold.ttf"),
+    PoppinsExtraBoldItalic: require("./assets/fonts/Poppins/Poppins-ExtraBoldItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <UserList />
